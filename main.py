9bcc044295cdfe460f9cc94c36cfdd13e97c0e57
Leon <LM28@hotmail.com>
2025-08-10 11:24:38 +0300

    .env file will be used just for local env. For docker it will be provided as an input file

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 #  for this purpose, the .env file should be uploaded to the machine where docker will run and please run the
 #  command below:
 # docker run --rm --env-file .env leonmaister/secret-extractor:1.0.0
+# to build: docker docker build . -t leonmaister/secret-extractor:1.0.0
+# #to push docker:
+# docker push leonmaister/secret-extractor:1.0.0
 
 import akeyless
 
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+if os.path.isfile(".env"):
+    path = find_dotenv()
+    load_dotenv(".env")
+
 
 ACCESS_ID = os.getenv("AKEYLESS_ACCESS_ID")
 ACCESS_KEY = os.getenv("AKEYLESS_ACCESS_KEY")
@@ -34,9 +41,8 @@ res = api.get_secret_value(body)
 print("Static Secret: " + res['/SecretExtractor/static-secret-text-format'])
 
 body = akeyless.GetDynamicSecretValue(name='/SecretExtractor/dynamic-secret-postgres', token=token)
-
 res = api.dynamic_secret_get_value(body)
-print(res['user'])
+
 print("Dynamic Secret Name: " + (res['user']))
 print("Dynamic Secret Password: " + (res['password']))
 
